659175504a2632e9dd7920ca4e24bbb7b1dc7ae4
Linh Pham <linh.pham@nashtechglobal.com>
2023-01-09 15:51:08 +0700

    update info generate table and add put, delete calls

--- src/main/java/nt/tshape/automation/reportmanager/HTMLReporter.java
+++ src/main/java/nt/tshape/automation/reportmanager/HTMLReporter.java
@@ -7,6 +7,8 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import lombok.SneakyThrows;
+import nt.tshape.automation.selenium.Utils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -113,12 +115,40 @@ public class HTMLReporter {
         return MarkupHelper.createCodeBlock(jsonObject, CodeLanguage.JSON);
     }
 
+    @SneakyThrows
+    public String markupCreateAPIInfoBlock(String requestType, String requestURL, String requestBody, String responseTime, String responseBody, String responseCode) {
+        String tableHTMLFrame = Files.readString(Paths.get("src/main/java/nt/tshape/automation/reportmanager/tableHTMLFrame.html"));
+        String randomID = Utils.generateRandomNumberInRange(100000, 999999);
+        String reqBodyElementID = "req_json_body_" + randomID;
+        String reqBodyScriptFunctionName = "req_function_" + randomID;
+        String resBodyElementID = "res_json_body_" + randomID;
+        String resBodyScriptFunctionName = "res_function_" + randomID;
+        responseTime += "ms";
+        switch (requestType) {
+            case "GET" -> requestType = "<span class='badge white-text green'>GET</span>";
+            case "POST" -> requestType = "<span class='badge white-text orange'>POST</span>";
+            case "PUT" -> requestType = "<span class='badge white-text blue'>PUT</span>";
+            case "DELETE" -> requestType = "<span class='badge white-text red'>DELETE</span>";
+        }
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%RequestType%", requestType);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%RequestURL%", requestURL);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%RequestBodyID%", reqBodyElementID);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%RequestBodyFunctionName%", reqBodyScriptFunctionName);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%RequestBodyContent%", requestBody);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%ResponseTime%", responseTime);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%ResponseBodyID%", resBodyElementID);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%ResponseBodyFunctionName%", resBodyScriptFunctionName);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%ResponseBodyContent%", responseBody);
+        tableHTMLFrame = tableHTMLFrame.replaceAll("%ResponseCode%", responseCode);
+        return tableHTMLFrame;
+    }
+
     public Markup markupRequestInfoTable(String requestType, String requestURL, String requestBody, String responseTimeInMiliSecond, String responseBody, String responseCode) {
         String[][] tableData = new String[4][3];
         tableData[0] = new String[]{"Request Type", "Request URL", "Request Body"};
         tableData[1] = new String[]{requestType, requestURL, requestBody};
         tableData[2] = new String[]{"Response Time", "Response Body", "Response Code"};
-        tableData[3] = new String[]{responseTimeInMiliSecond+"ms", responseBody, responseCode};
+        tableData[3] = new String[]{responseTimeInMiliSecond + "ms", responseBody, responseCode};
         return MarkupHelper.createTable(tableData);
     }
 }
